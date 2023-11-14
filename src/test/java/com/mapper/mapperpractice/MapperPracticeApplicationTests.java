024aeec26f73162f695137647da96d2d27c43284
chie-hira <capella.a36@gmail.com>
2023-11-14 22:50:48 +0900

    first-commit

---
+++ src/test/java/com/mapper/mapperpractice/MapperPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapper.mapperpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapperPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
