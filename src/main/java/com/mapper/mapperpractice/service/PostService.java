ddd4c4d1ecf838ca11628aff7077bd46cc477dfe
chie-hira <capella.a36@gmail.com>
2023-11-18 00:32:14 +0900

    PostServiceを作成

---
+++ src/main/java/com/mapper/mapperpractice/service/PostService.java
@@ -0,0 +1,23 @@
+package com.mapper.mapperpractice.service;
+
+import com.mapper.mapperpractice.entity.Post;
+import com.mapper.mapperpractice.mapper.PostMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service //サービスだよ宣言
+public class PostService {
+    // final　定数
+    private final PostMapper postMapper;
+
+    //コンストラクタインジェクション　呼び出して部品を使えるようにする
+    public PostService(PostMapper postMapper) {
+        this.postMapper = postMapper;
+    }
+
+    public List<Post> getPosts() {
+        List<Post> posts = postMapper.findAll();
+        return posts;
+    }
+}
